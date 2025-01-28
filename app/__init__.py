f9dcae35a871abc7fd99cf43628887ba1156cdbf
LySingLim <100894942+LySingLim@users.noreply.github.com>
2025-01-28 20:30:45 +0700

    Add .gitignore and clean up repository

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,22 @@
 from flask import Flask
 from .routes import register_routes
+import time
+import os  # Import the os module
 
 def create_app():
-    app = Flask(__name__, static_folder='../static', template_folder='../templates')
+    # Resolve the absolute path for static and template folders
+    static_folder = os.path.abspath('./static')
+    template_folder = os.path.abspath('./templates')
+
+    app = Flask(__name__, static_folder=static_folder, template_folder=template_folder)
+
+    # Disable static file caching in development
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
     register_routes(app)
+
+    @app.context_processor
+    def inject_time():
+        return {'time': time}
+
     return app
--- main.py
+++ main.py
@@ -4,3 +4,11 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.after_request
+def add_header(response):
+    # Disable caching for static files
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
