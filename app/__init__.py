4cb704906e23d8cab9c74f0556899494e7875ade
LySingLim <100894942+LySingLim@users.noreply.github.com>
2025-01-28 20:53:35 +0700

    tailwind fixed

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,27 @@
 from flask import Flask
 from .routes import register_routes
 import time
-import os  # Import the os module
+import os
 
 def create_app():
-    # Resolve the absolute path for static and template folders
+    # Set up Flask application
     static_folder = os.path.abspath('./static')
     template_folder = os.path.abspath('./templates')
 
     app = Flask(__name__, static_folder=static_folder, template_folder=template_folder)
-
-    # Disable static file caching in development
-    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # Disable static file caching
+    # Ensure templates auto-reload in development
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
 
     register_routes(app)
+
+    @app.after_request
+    def add_header(response):
+        # Disable caching for all requests
+        response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+        return response
 
     @app.context_processor
     def inject_time():
